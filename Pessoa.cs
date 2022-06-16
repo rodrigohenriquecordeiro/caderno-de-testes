f426b036a3ffdd1b666aad832bf841bb5f8155d4
Rodrigo H. Cordeiro <rodrigohenriquecordeiro@gmail.com>
2022-06-16 09:52:21 -0300

    Teste com arquivo .gitignore

---
+++ Pessoa.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CadernoTestes
+{
+    class Produto
+    {
+        public string Nome;
+        public double Preco;
+        public int Quantidade;
+        public Produto()
+        {
+            Quantidade = 0;
+        }
+        public Produto(string nome, double preco) : this()
+        {
+            Nome = nome;
+            Preco = preco;
+        }
+        public Produto(string nome, double preco, int quantidade) : this(nome, preco)
+        {
+            Quantidade = quantidade;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CadernoTestes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var sul = new List<string>{"Paraná", "Santa Catarina", "Rio Grande do Sul"};
+
+            foreach (var estado in sul)
+            {
+                Console.WriteLine(estado.ToUpper().Trim());
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
